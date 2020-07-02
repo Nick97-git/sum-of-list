1a89fd1d840f3f723bebe8e58c585ea3149a96b8
Nick97-git <nickarchangel97@gmail.com>
2020-07-02 12:03:16 +0300

    relocated logic of Executor Service to SumExecutorService

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,9 @@
-import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 import java.util.concurrent.ForkJoinPool;
-import java.util.concurrent.Future;
 import java.util.stream.Collectors;
 import java.util.stream.LongStream;
 
 public class Main {
-    public static final int NUM_OF_THREADS = 4;
     public static final int NUM_OF_ELEMENTS = 1_000_000;
 
     public static void main(String[] args) throws InterruptedException {
@@ -25,25 +18,11 @@ public class Main {
         long forkEnd = System.currentTimeMillis();
         System.out.println("ForkJoin: " + forkJoinSum + ", time: " + (forkEnd - forkStart));
 
-        ExecutorService executorService = Executors.newFixedThreadPool(NUM_OF_THREADS);
-        List<Callable<Long>> callableList = new ArrayList<>();
-        int n = nums.size() / 10;
-        for (int i = 0; i < 10; i++) {
-            Callable<Long> callable = new CallableSum(nums.subList(i * n, (n * (i + 1))));
-            callableList.add(callable);
-        }
+        SumExecutorService sumExecutorService = new SumExecutorService(nums, 4);
         long executorStart = System.currentTimeMillis();
-        List<Future<Long>> futures = executorService.invokeAll(callableList);
-        long executorSum = futures.stream()
-                .mapToLong(future -> {
-                    try {
-                        return future.get();
-                    } catch (InterruptedException | ExecutionException e) {
-                        return 0;
-                    }
-                }).sum();
+        long sum = sumExecutorService.calculateListSum();
         long executorEnd = System.currentTimeMillis();
-        executorService.shutdown();
-        System.out.println("Executor: " + executorSum + ", time: " + (executorEnd - executorStart));
+        sumExecutorService.shutdown();
+        System.out.println("Executor: " + sum + ", time: " + (executorEnd - executorStart));
     }
 }
